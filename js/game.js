fe55a3923c403c4a882711e48356389529cd43e9
SConvertino <stefano.convertino@gmail.com>
2017-08-03 13:21:12 +0200

    modified

--- js/game.js
+++ js/game.js
@@ -2,16 +2,14 @@ Game = {};
 var w = 600;
 var h = 200;
 var playerTint= getRandomColor();
-var playerSize=1;
+var playerScore=3;
+var fps=0;
+var oldCube=0;
+var healtBarSize=0;
 /*================================================================ UTIL
 */
-
 function randInt(num) {
   return Math.floor(Math.random() * num)
-};
-
-function randDoub(num) {
-  return Math.random() * num
 };
 
 function getRandomColor() {
@@ -23,11 +21,11 @@ function getRandomColor() {
   return color;
 }
 
-/*================================================================ LOAD
-*/
 
-Game.Load = function(game) {};
-Game.Load.prototype = {
+/*================================================================ PLAY
+*/
+Game.Play = function(game) {};
+Game.Play.prototype = {
   preload: function() {
     game.stage.backgroundColor = '#000000';
     game.load.image('player', 'images/player.png');
@@ -38,25 +36,22 @@ Game.Load.prototype = {
     game.load.audio('jump', 'sounds/jump.wav');
   },
   create: function() {
-    game.state.start('Play');
-  }
-};
+        var bmd = game.add.bitmapData(200,40);
+         bmd.ctx.beginPath();
+         bmd.ctx.rect(0,0,720,15);
+         bmd.ctx.fillStyle = '#00685e';
+         bmd.ctx.fill();
+         this.healthBar = game.add.sprite(game.world.centerX-100,game.world.centerY/3,bmd);
+         this.healtBarSize = this.healthBar.width; 
+         this.healthBar.width = (this.healtBarSize/5)*playerScore;
+         this.healthBar.anchor.y = 0.5;
 
-/*================================================================ PLAY
-*/
-
-Game.Play = function(game) {};
-Game.Play.prototype = {
-  render: function() {
-    //this.updateDebug();
-  },
-  resize: function() {
-    //console.log('resize');
-  },
-  shutdown: function() {
-    //console.log('shutdown');
-  },
-  create: function() {
+    this.labelTuto = game.add.text(Math.floor(w / 2) + 0.5, h - 35 + 0.5, 'Press the space bar to Start!', {
+        font: '18px Arial',
+        fill: '#fff',
+        align: 'center'
+      });
+    this.labelTuto.anchor.setTo(0.5, 0.5);
     // player
     this.player = this.game.add.sprite(80, h * 2 / 3 - 20, 'player');
     this.game.physics.arcade.enable(this.player);
@@ -64,61 +59,62 @@ Game.Play.prototype = {
     this.player.body.gravity.y = 10000;
     this.player.anchor.setTo(0.5, 0.5);
     this.player.tint =playerTint;
-
     // cube
     this.cubes = game.add.group();
-    //this.cubes.createMultiple(20, 'cube');
     // line
     this.line = this.game.add.sprite(w / 2, Math.floor(h * 2 / 3), 'line');
     this.game.physics.arcade.enableBody(this.line);
     this.line.anchor.setTo(0.5, 0.2);
     this.line.body.immovable = true;
-
-    this.labelTuto = game.add.text(Math.floor(w / 2) + 0.5, h - 35 + 0.5, 'When the cube touch the ground press space to jump!', {
-          font: '18px Arial',
-          fill: '#fff',
-          align: 'center'
-        });
-        this.labelTuto.anchor.setTo(0.5, 0.5);
     // sound
     this.hitSound = game.add.audio('hit');
     this.jumpSound = game.add.audio('jump');
- 
-    // input
-    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
     // emitter
     this.emitter = game.add.emitter(0, 0, 200);
     this.emitter.makeParticles('pixel');
     this.emitter.gravity = 0;
     this.emitter.minParticleSpeed.setTo(-200, -200);
     this.emitter.maxParticleSpeed.setTo(200, 200);
-
+    // input
+    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     // load game
-    this.loadGame();
+    this.spaceKey.onDown.add(this.loadGame, this);
+  },
+  loadGame: function(){ 
+    this.spaceKey.onDown.remove(this.loadGame, this); 
+    this.playerScore = 2;
+    if(this.labelTuto){
+      this.labelTuto.destroy();
+    }
+    this.labelTuto = game.add.text(Math.floor(w / 2) + 0.5, h - 35 + 0.5, 'Press space to jump!', {
+          font: '18px Arial',
+          fill: '#fff',
+          align: 'center'
+        });
+    this.labelTuto.anchor.setTo(0.5, 0.5);
+    if (this.player.y > this.line.y) {
+      this.initPlayer();
+    }
+    this.drawCube(300);
+    this.drawCube(550);
+
+  }, 
+  initPlayer: function() {
+      this.player.body.gravity.y = 0;
+      this.player.x = 60;
+      this.player.y = h * 2 / 3 - this.player.height / 2 - 30;
+      this.player.angle = 0;
   },
   update: function() {
     game.physics.arcade.collide(this.player, this.line);
-   // on the ground && press space
-  /* if(this.player.alive && this.isStarted){
-
-      this.isStarted = true
-   }*/
     if (this.spaceKey.isDown && this.player.body.touching.down) {
       this.hitSound.play('', 0, 0.2);
       this.playerJump(-400);
-      if (!this.isStarted) {
-        this.isStarted = true
-        //game.add.audio('music').play('', 0, 0.1, true);
-      }
     }else{
       if(this.player.body.touching.down){
-        this.playerJump(-250);
+        this.playerJump(-70);
       } 
     }
-  /*  if(this.player.body.touching.down && this.isStarted) {
-      this.player.alive = true;
-    }*/
     this.cubes.forEachAlive(function(cube) {
       if(cube.x<60){
         this.drawCube(550);
@@ -128,80 +124,57 @@ Game.Play.prototype = {
     this.player.body.gravity.y = 800;
     game.physics.arcade.overlap(this.player, this.cubes, this.playerHit, null, this);
   },
-
-  loadGame: function(){ 
-    if (this.player.y > this.line.y) {
-      this.initPlayer();
-    }
-    this.drawCube(300);
-    this.drawCube(550);
-  },
   playerJump: function(jumpSpeed) {
     this.player.body.velocity.y = jumpSpeed;
-    // spin the player
     this.rotation = this.game.add.tween(this.player).to({
       angle: this.player.angle + 180
     }, 0, Phaser.Easing.Linear.None);
     this.rotation.start();
   },
   playerHit: function(player, hit) {
-   // if (this.player.alive) {
-    this.hitSound.play('', 0, 0.2);
-
-    if(player.tint==hit.tint){
-      this.emitter.x = player.x + player.width / 2;
-      this.emitter.y = player.y + player.height / 2;
-      this.emitter.start(true, 300, null, 8);  
-      playerSize=playerSize*1.1;
-      this.game.add.tween(player.scale).to({
-          y: playerSize,
-          x: playerSize
-        }, 6, Phaser.Easing.Linear.None).start();
-      this.playerJump(-400);
-    
-    }else{
-      if(playerSize>0.5){
-        playerSize=playerSize*0.5;
-        this.game.add.tween(player.scale).to({
-            y: playerSize,
-            x: playerSize
-        }, 6, Phaser.Easing.Linear.None).start();
-      }else{
-        if(this.player.alive){
-          this.player.alive = false;  
-          this.labelTuto = game.add.text(Math.floor(w / 2) + 0.5, h - 35 + 0.5, 'When the cube touch the ground press space to jump!', {
-            font: '18px Arial',
-            fill: '#fff',
-            align: 'center'
-          });
-          this.labelTuto.anchor.setTo(0.5, 0.5);
-        }
-      }
-    }
-  },
-  initPlayer: function() {
-      this.player.body.gravity.y = 0;
-      this.player.x = 60;
-      this.player.y = h * 2 / 3 - this.player.height / 2 - 30;
-      this.player.angle = 0;
-
-      if (this.rotation) {
-        this.rotation.pause();
-      }
+    if(oldCube!=hit.id){
+        oldCube=hit.id;
+        this.hitSound.play('', 0, 0.2);
+        if(player.tint==hit.tint){
+          if(this.playerScore<5){
+            this.playerScore++;
+          }
+          this.emitter.x = player.x + player.width / 2;
+          this.emitter.y = player.y + player.height / 2;
+          this.emitter.start(true, 300, null, 8);  
+          this.playerJump(-400);
+        }else{
+          if(this.playerScore>0){
+            this.playerScore--;
+          }else{
+            this.labelTuto.destroy();
+            this.labelTuto = game.add.text(Math.floor(w / 2) + 0.5, h - 35 + 0.5, 'Press the spacebar to restart!', {
+              font: '18px Arial',
+              fill: '#fff',
+              align: 'center'
+            });
+            this.labelTuto.anchor.setTo(0.5, 0.5);
+            this.cubes.removeAll();
+            this.spaceKey.onDown.add(this.loadGame, this);
+          }
+        }        
+        this.healthBar.width = (this.healtBarSize /5)*(this.playerScore);
+     }
   },
   //I need to change this ...
   drawCube: function(x) {
     var cube;
-    var height=randDoub(100);
-    var width=randDoub(100);
+    var height=100;
+    var width=100;
   
     this.cube = this.game.add.sprite(height,width, 'cube');
     this.cube.x = x;
     this.cube.y = this.line.y-this.cube.height;
     this.game.physics.arcade.enable(this.cube);
     this.cube.body.velocity.x = -150;
+    this.cube.id=randInt(100000);
     this.player.body.gravity.y = 100;
-    if(randInt(2)){
+    if(randInt(5)>1){
       this.cube.tint= getRandomColor();
     }else{
       this.cube.tint= this.player.tint;
@@ -209,19 +182,8 @@ Game.Play.prototype = {
     this.cubes.add(this.cube);
   }
 };
-
-/*================================================================ END
-*/
-
-Game.End = function(game) {};
-Game.End.prototype = {
-  create: function() {
-    console.log("create");
-  }
-};
+ 
 
 var game = new Phaser.Game(w, h, Phaser.AUTO, 'game-box');
-game.state.add('Load', Game.Load);
 game.state.add('Play', Game.Play);
-game.state.add('End', Game.End);
-game.state.start('Load');
+game.state.start('Play');
